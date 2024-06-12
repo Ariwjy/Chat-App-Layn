19801c1db38b0b22a9f4b57d3425006cb9794d40
Nicholas-Eugene <nicholas.eugene021204@gmail.com>
2024-06-12 15:42:18 +0700

    updated Message Model

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,39 +1,39 @@
 class Message {
   Message({
+    required this.toId,
     required this.msg,
     required this.read,
-    required this.told,
     required this.type,
-    required this.fromID,
+    required this.fromId,
     required this.sent,
   });
 
+  late final String toId;
   late final String msg;
   late final String read;
-  late final String told;
-  late final String type;
-  late final String fromID;
+  late final String fromId;
   late final String sent;
   late final Type type;
-  
-  Message.fromJson(Map<String, dynamic> json){
+
+  Message.fromJson(Map<String, dynamic> json) {
+    toId = json['toId'].toString();
     msg = json['msg'].toString();
     read = json['read'].toString();
-    told = json['told'].toString();
     type = json['type'].toString() == Type.image.name ? Type.image : Type.text;
-    fromID = json['fromID'].toString();
+    fromId = json['fromId'].toString();
     sent = json['sent'].toString();
   }
 
   Map<String, dynamic> toJson() {
     final data = <String, dynamic>{};
+    data['toId'] = toId;
     data['msg'] = msg;
     data['read'] = read;
-    data['told'] = told;
     data['type'] = type.name;
-    data['fromID'] = fromID;
+    data['fromId'] = fromId;
     data['sent'] = sent;
     return data;
   }
 }
-  enum Type{text, image}
+
+enum Type { text, image }
